fb00da6fc5db7c86031e7d44b94a5a6f9dc25d1b
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-01-17 09:59:58 +0100

    updated joker script to include resolve

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,7 +1,11 @@
-import joker from 'give-me-a-joke';
-
 export function dadJoke() {
   return new Promise(function (resolve, reject) {
-    joker.getRandomDadJoke(resolve);
+    try {
+      joker.getRandomDadJoke(function (joke) {
+        resolve(joke);
+      });
+    } catch (error) {
+      reject(error);
+    }
   });
 }
